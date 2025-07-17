b1c9c637eac87ddde45b8f899a78ce7827bd4b7f
SangHyun Jung <wjdtkdgus876@naver.com>
2025-07-17 22:53:25 +0900

    fix: 근무자 출력 메소드 리팩토링

--- src/main/java/oncall/view/OutputView.java
+++ src/main/java/oncall/view/OutputView.java
@@ -14,45 +14,24 @@ public class OutputView {
     /*
     ----------------------------------- 근무자 출력 메소드 -----------------------------------
     */
-    // todo 출력메소드 solid 원칙 적용
-    // param : 배치된 근무자 / 월 / 요일
-    // 내부에서 그 월에 해당하는 휴일도 가져오기
-    // assignWorkers메소드 보고 똑같이 출력
 
-    // totalWorkersOrder를 차례대로 출력...
     public static void printMonthlyTable(List<String> totalWorkersOrder, int month, String day) {
-        String sentence;
-        String holiday = "(휴일)";
-        String worker;
 
         for (int date = 1, order = 0; date <= Month.getDaysInMonth(month); date++, order++) {
+            // 공휴일 여부
+            boolean isHoliday = Month.getHolidays(month).contains(date);
+            String worker = totalWorkersOrder.get(order);
 
-            // 주말 출력
-            if (day.equals(Day.SAT.getDay()) || day.equals(Day.SUN.getDay()))
-            {
-                worker = totalWorkersOrder.get(order);
-                sentence = month+"월 " + date +"일 " + day + " " + worker;
-
-                System.out.println(sentence);
-                day = Day.getNextDay(day);
-                continue;
-            }
-
-            // 공휴일 출력
-            if (Month.getHolidays(month).contains(date)) {
-                worker = totalWorkersOrder.get(order);
-                sentence = month+"월 " + date +"일 " + day + holiday + " " + worker;
-
-                System.out.println(sentence);
-                day = Day.getNextDay(day);
-                continue;
-            }
-
-            worker = totalWorkersOrder.get(order);
-            sentence = month+"월 " + date +"일 " + day + " " + worker;
-
+            String sentence = makeSentence(worker, month, day, date, isHoliday);
             System.out.println(sentence);
+
             day = Day.getNextDay(day);
         }
+    }
+
+    public static String makeSentence(String worker, int month, String day, int date, boolean isHoliday) {
+        String holiday = isHoliday ? "(휴일)" : "";
+
+        return month + "월 " + date + "일 " + day + holiday + " " + worker;
     }
 }
